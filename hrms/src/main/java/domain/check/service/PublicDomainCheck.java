aa4eba1b0b7c6cdbc09aef95718cf4138c0b9003
Kadir Şükrü Gök <83140154+KdrSkrGok78@users.noreply.github.com>
2021-05-29 18:35:28 +0300

    Delete hrms directory

--- hrms/src/main/java/domain/check/service/PublicDomainCheck.java
+++
@@ -1,8 +0,0 @@
-package domain.check.service;
-
-public class PublicDomainCheck {
-
-	public void checkDomain(String domainAdress) {
-		System.out.println("domain doğrulandı : "+domainAdress);
-	}
-}
